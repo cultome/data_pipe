7ca95c91d5728f1469018fe874e1e51595691cc0
Carlos Soria <csoria@cultome.io>
2018-08-27 16:35:34 -0500

    Load steps without includeing module

--- lib/data_pipe.rb
+++ lib/data_pipe.rb
@@ -14,54 +14,6 @@ module DataPipe
     instance
   end
 
-  def self.included(clazz)
-    steps_base = "#{__dir__}/data_pipe/step"
-
-    def get_dir_files_rec(dirname)
-      Dir.entries(dirname)
-        .select{|filename| filename !~ /^\.\.?$/}
-        .reduce([]) do |acc,child|
-          acc << File.join(dirname, child)
-          acc
-        end
-    end
-
-    Dir.entries(steps_base)
-      .select{|filename| filename !~ /^\.\.?$/}
-      .flat_map do |filename|
-        relative_filename = File.join(steps_base, filename)
-        if File.directory?(relative_filename)
-          get_dir_files_rec(relative_filename)
-        else
-          [relative_filename]
-        end
-      end
-      .select{|filename| filename.end_with?(".rb") }
-      .map{|filename| filename.delete_suffix(".rb").delete_prefix(steps_base + "/") }
-      .each{|name| require "data_pipe/step/#{name}" }
-      .map{|name| "DataPipe::Step::#{name.camelize}".constantize }
-      .each do |step|
-        instance = step.new
-        next unless instance.respond_to? :step_command
-
-        cmd_name = instance.step_command
-        Pipe.define_method cmd_name do |args={}, &blk|
-          local_instance = step.new
-          pipe << local_instance.prepare(args, &blk)
-        end
-      end
-      .each do |step|
-        instance = step.new
-        next unless instance.respond_to? :helper_command
-
-        cmd_name = instance.helper_command
-        Pipe.define_method cmd_name do |args={}, &blk|
-          local_instance = step.new
-          local_instance.prepare(args, &blk).send(cmd_name)
-        end
-      end
-  end
-
   class Pipe
     include Loggable
 
@@ -88,3 +40,49 @@ module DataPipe
     end
   end
 end
+
+->() {
+  steps_base = "#{__dir__}/data_pipe/step"
+
+  def get_dir_files_rec(dirname)
+    Dir.entries(dirname)
+      .select{|filename| filename !~ /^\.\.?$/}
+      .reduce([]) do |acc,child|
+      acc << File.join(dirname, child)
+      acc
+    end
+  end
+
+  Dir.entries(steps_base)
+    .select{|filename| filename !~ /^\.\.?$/}
+    .flat_map {|filename|
+    relative_filename = File.join(steps_base, filename)
+    if File.directory?(relative_filename)
+      get_dir_files_rec(relative_filename)
+    else
+      [relative_filename]
+    end
+  }.select{|filename| filename.end_with?(".rb") }
+    .map{|filename| filename.delete_suffix(".rb").delete_prefix(steps_base + "/") }
+    .each{|name| require "data_pipe/step/#{name}" }
+    .map{|name| "DataPipe::Step::#{name.camelize}".constantize }
+    .each {|step|
+    instance = step.new
+    next unless instance.respond_to? :step_command
+
+    cmd_name = instance.step_command
+    DataPipe::Pipe.define_method cmd_name do |args={}, &blk|
+      local_instance = step.new
+      pipe << local_instance.prepare(args, &blk)
+    end
+  }.each { |step|
+    instance = step.new
+    next unless instance.respond_to? :helper_command
+
+    cmd_name = instance.helper_command
+    DataPipe::Pipe.define_method cmd_name do |args={}, &blk|
+      local_instance = step.new
+      local_instance.prepare(args, &blk).send(cmd_name)
+    end
+  }
+}.call()
