d05c6cc8c7a3626106eb50118465f4d62a0eb1d7
Carlos Soria <csoria@cultome.io>
2018-08-27 00:33:43 -0500

    Add file option to CSV writer

--- spec/data_pipe/csv_files_spec.rb
+++ spec/data_pipe/csv_files_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "Handle of CSV files" do
       DataPipe.create do
         log_to StringIO.new
 
-        load_from_csv file: "spec/sample/1.csv"
+        load_from_csv file: "spec/sample/1.csv", headers: false
         write_to_csv stream: output
       end.process!
 
@@ -66,4 +66,30 @@ date,string,int,float
       FILE
     end
   end
+
+  context "writes to a file" do
+    before :each do
+      File.delete("tmp.csv") if File.exists?("tmp.csv")
+    end
+
+    after :each do
+      File.delete("tmp.csv") if File.exists?("tmp.csv")
+    end
+
+    it "writes to a file" do
+      DataPipe.create do
+        log_to StringIO.new
+
+        load_from_csv file: "spec/sample/1.csv"
+        write_to_csv file: "tmp.csv"
+      end.process!
+
+      file_content = open("tmp.csv").read
+      expect(file_content).to eq <<-FILE
+date,string,int,float
+2118-12-31,Carlos,34,1.85
+      FILE
+    end
+  end
+
 end
