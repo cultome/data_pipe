0e003bd4d588d0fb8b0a6fd7bc5630966e167e0d
Carlos Soria <csoria@cultome.io>
2018-08-27 10:57:54 -0500

    Implement file and stream parameters

--- lib/data_pipe/step/json_loader.rb
+++ lib/data_pipe/step/json_loader.rb
@@ -11,8 +11,14 @@ module DataPipe::Step
     end
 
     def iter
+      if params.respond_to? :stream
+        input_source = params.stream.read
+      else
+        input_source = open(params.file, "r").read
+      end
+
       Enumerator.new do |rsp|
-        JSON.load(open(params.stream)).each do |obj|
+        JSON.load(input_source).each do |obj|
           rsp << Record.new(obj, headers: true)
         end
       end
